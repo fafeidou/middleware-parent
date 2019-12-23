aac216eaab04637685d0247c8e1322661d6283cf
qfx <qinfuxiang>
2019-12-23 18:07:10 +0800

    add redis likes

---
+++ middleware-redis/redis-likes/src/main/java/com/batman/redislikes/RedisLikesApplication.java
@@ -0,0 +1,13 @@
+package com.batman.redislikes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisLikesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisLikesApplication.class, args);
+	}
+
+}
---
+++ middleware-redis/redis-likes/src/test/java/com/batman/redislikes/RedisLikesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batman.redislikes;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisLikesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
